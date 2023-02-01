af7a77aaac59f9a5c2e40293fb9c0ad885f3a754
JAE YEON KIM <65452447+jaethewiederholen@users.noreply.github.com>
2023-02-02 00:20:47 +0900

    Feat: connect wallet

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from "react";
 import "./nav.css";
 import { ReactComponent as Quinoalogo } from "../components/asset/quinoa_logo.svg";
-function Nav() {
+
+const Nav = ({
+  currentAccount,
+  correctNetwork,
+  connectWallet,
+  changeNetwork,
+}: any) => {
+  const formatAddress = (address: string) => {
+    if (address.slice(-4) === ".eth") return address;
+    return address.slice(0, 4) + "..." + address.slice(-4);
+  };
+
   const [scrollPosition, setScrollPosition] = useState(0);
   const updateScroll = () => {
     setScrollPosition(window.scrollY || document.documentElement.scrollTop);
@@ -13,11 +24,21 @@ function Nav() {
     <header id={scrollPosition < 100 ? "og_header_wrap" : "og_header_wrap"}>
       <div className="header">
         <Quinoalogo className="logo"></Quinoalogo>
-        <div className="wallet_btn">
-          <span className="get_start">Get start</span>
-        </div>
+        {currentAccount === undefined ? (
+          <div onClick={connectWallet} className="wallet_btn">
+            <span className="get_start">Get start</span>
+          </div>
+        ) : !correctNetwork ? (
+          <div onClick={changeNetwork} className="wallet_btn">
+            <span className="get_start">Change network</span>
+          </div>
+        ) : (
+          <div className="wallet_btn">
+            <span className="get_start">{formatAddress(currentAccount)}</span>
+          </div>
+        )}
       </div>{" "}
     </header>
   );
-}
+};
 export default Nav;
