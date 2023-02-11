530733dc5bf8af54e16733bea9898e6fe82ecbb4
wakii <cwh6997@gmail.com>
2023-02-12 03:00:16 +0900

    add toast, useSell

---
+++ src/hooks/useSell.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect, useCallback } from "react";
+import { BigNumberish, ethers } from "ethers";
+import product_abi from "../abis/IProduct.json";
+import erc20_abi from "../abis/ERC20.json";
+
+
+
+export const useSell = (amount: any, assetAddress: any, currentAccount: any, ethereum: Window["ethereum"]) => {
+    const signer = new ethers.providers.Web3Provider(ethereum).getSigner();
+    const [sellTxStatus, setTxStatus] = useState<string>("default");
+    const sell = useCallback(
+        async (amount: any, assetAddress: any) => {
+            const productAddress: string = process.env.REACT_APP_PRODUCT_ADDRESS || "";
+            const product = new ethers.Contract(
+                productAddress,
+                product_abi.abi,
+                signer
+              );
+              try {
+                console.log(amount,assetAddress);
+                console.log("SELLLLLLLL")
+                const mintTx = await product.withdraw(assetAddress, ethers.utils.parseUnits(amount.toString()), currentAccount, currentAccount);
+                console.log("SELL")
+                console.log(mintTx);
+                setTxStatus("pending");
+                const receipt = await mintTx.wait();
+                console.log(!!receipt.blockHash ? "success" : "error");
+                setTxStatus(!!receipt.blockHash ? "success" : "error");
+            }
+            
+            catch(e) {
+                console.log(e);
+                setTxStatus("error");
+            }
+        },
+    [amount, assetAddress, currentAccount, signer]);
+    return {sell, sellTxStatus};
+};
